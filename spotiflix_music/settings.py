c1602861e296340c7b35f503a52d82edfc3e7e57
Dimifabbridf96 <dimi.fabbri@gmail.com>
2022-10-20 15:18:07 +0000

    test with Procfile

--- spotiflix_music/settings.py
+++ spotiflix_music/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://spotiflix-app.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ["spotiflix-app.herokuapp.com", "localhost"]
 
 
 # Application definition
