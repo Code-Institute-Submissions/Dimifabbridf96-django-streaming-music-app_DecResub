f553757a34457e3abfbdd151330e6d4dec7ab4a1
Dimifabbridf96 <dimi.fabbri@gmail.com>
2022-10-20 14:24:06 +0000

    Set up completed for deployment on Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web:gunicorn spotiflix_music.wsgi
---
+++ streaming_music/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class StreamingMusicConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'streaming_music'
