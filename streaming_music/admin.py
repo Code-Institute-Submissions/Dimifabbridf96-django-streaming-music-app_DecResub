474e9681d6a3dc63854617ebfc97668931acdb5d
Dimifabbridf96 <dimi.fabbri@gmail.com>
2022-10-22 17:23:25 +0000

    Add Artist model and migrate

--- streaming_music/admin.py
+++ streaming_music/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Artist
+from . import models
 # Register your models here.
 
 admin.site.register(Artist)
+admin.site.register(Album)
+admin.site.register(Song)
+admin.site.register(Genre)
--- streaming_music/models.py
+++ streaming_music/models.py
@@ -1,12 +1,54 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 
 class Artist(models.Model):
-    name = models.CharField(max_length=50)
-    surname = models.CharField(max_length=50)
+    artist_name = models.CharField(max_length=50)
+    artist_surname = models.CharField(max_length=50)
+
+    def __str__(self):
+        return artist_name + artist_surname
+
+class Album(model.Model):
+    album_artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name = 'album_artist')
+    album_title = models.CharField(max_length=200, unique=True)
+    created_on = models.DateField()
+    genre = models.ForeignKey(Genre)
+    songs = models.ForeignKey(Song)
+    album_image = CloudinaryField('image', default='image')
+
+    def __str__(self):
+        return album_title
 
 
-    class Meta:
-        ordering = ['name']
+class Genre(models.Model):
+    GENRE = (
+        ('R','Rock'),
+        ('J','Jazz'),
+        ('P','Pop'),
+        ('H','House'),
+        ('B','Blues'),
+        ('M','Metal'),
+
+    )
+    genre = models.CharField(max_length=1, choices=GENRE)
+    other = models.CharField(max_length=20)
+
+    def __str__(self):
+        return genre
+
+class Song(models.Model):
+    song_title = models.CharField(max_length=50, unique=True)
+    song_artist = models.ForeignKey(Artist, on_delete=models.CASCADE, related_name = 'song_artist')
+    uploaded_on = models.DateTimeField(auto_now=True)
+    song_album = models.ForeignKey(Album, on_delete=models.on_delete.CASCADE, related_name = 'song_album')
+
+    def __str__(self):
+        return song_title
+
+    
+
+
+
 
