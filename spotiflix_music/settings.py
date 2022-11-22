1120c57e269d435486bc1e151fa4fbe4691dda01
Dimifabbridf96 <dimi.fabbri@gmail.com>
2022-11-22 18:20:43 +0000

    working on s3 proper function

--- spotiflix_music/settings.py
+++ spotiflix_music/settings.py
@@ -51,7 +51,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_summernote',
     'crispy_forms',
-    'custom.storages.py'
     'streaming_music',
     'storages',
 ]
@@ -166,8 +165,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 if 'USE_AWS' in os.environ:
     # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000',
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000',
     }
 
     # Bucket Config
